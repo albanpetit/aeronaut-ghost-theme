685dcf8b6f1018ac8ba21cc94e23569266c803b7
albanpetit <contact@albanpetit.com>
2024-06-02 19:16:43 +0000

    style: daisyui theme change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./*.hbs", "./**/*.hbs"],
   theme: {
       extend: {}
   },
-  plugins: [require("@tailwindcss/typography"), require('daisyui'),]
+  plugins: [require("@tailwindcss/typography"), require('daisyui'),],
+  daisyui: {
+    themes: ["nord"]
+  }
 }
