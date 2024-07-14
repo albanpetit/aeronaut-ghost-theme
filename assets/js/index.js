66c6b78265739fe347f01fec2c25e0fa2dc373bd
Alban Petit <contact@albanpetit.com>
2024-07-14 15:52:42 +0000

    fix: css call in javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,8 @@
 // Import CSS
 import "../css/index.css";
-import "../css/ghost-content.css"
+import "../css/bookmark.css";
+import "../css/callout.css";
+import "../css/gallery.css";
+import "../css/ghost.css";
 
 // Import JS
