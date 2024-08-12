37277855b8d6a911469ff3d6d77051238f8555be
Alban Petit <contact@albanpetit.com>
2024-08-12 19:25:44 +0000

    refactor: change ghost content css file name

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,9 @@
 // Import CSS
 import "../css/index.css";
+import "../css/announcement-bar.css";
 import "../css/bookmark.css";
 import "../css/callout.css";
 import "../css/gallery.css";
-import "../css/ghost.css";
+import "../css/ghost-content.css";
 
 // Import JS
