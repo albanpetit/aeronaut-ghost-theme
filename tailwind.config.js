4d0316f4dd029c805a5f08742c219d8bcd588482
Alban Petit <contact@albanpetit.com>
2024-08-17 20:15:29 +0000

    style: change global screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 export default {
   content: ["./*.hbs", "./**/*.hbs"],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       aspectRatio: {
           'a4': '21 / 29.7',
